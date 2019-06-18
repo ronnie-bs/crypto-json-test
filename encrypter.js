2a808ba7577013f2ed327a35d3e8346153a2d160
Ronnie Mathew <ronnie@bricksimple.com>
2019-06-18 11:32:05 -0400

    Made modifications to convert the JSON in the clear with encrypted data

--- encrypter.js
+++ encrypter.js
@@ -11,6 +11,12 @@ const CRYPTO_KEYS = [
     'id', 'type', 'firstName', 'lastName', 'effectiveDateTime'
 ];
 
+const FILE_PATH = 'data';
+const INPUT_FILENAME  = 'SP-Search-Results-Un-encrypted.json'
+const OUTPUT_FILENAME = 'SP-Search-Results.json';
+const INPUT_PATH  = path.join(__dirname, FILE_PATH, INPUT_FILENAME);
+const OUTPUT_PATH = path.join(__dirname, FILE_PATH, OUTPUT_FILENAME);
+
 function encrypt(input) {
     return cryptoJSON.encrypt(input, CRYPTO_PASSWORD, {
         encoding: CRYPTO_ENCODING,
@@ -19,62 +25,40 @@ function encrypt(input) {
     });
 }
 
-function mockMain() {
-    const input = {
-        "id": "1459",
-        "title": "Personal Assistant Under Supervision I",
-        "description": "Working under direct supervision or working autonomously on routine assignments (4 positions)",
-        "usageCount": 0,
-        "status": "PUBLISHED",
-        "locale": "en",
-        "jobRoleTypeId": "ASB01",
-        "standardHayGrade": "8",
-        "levelName": "Individual Contributor",
-        "familyName": "Administration / Support / Service",
-        "subFamilyName": "Secretarial",
-        "accessRoles": "READ",
-        "isTemplateJob": true,
-        "profileType": "BEST_IN_CLASS",
-        "isProfileInProfileCollection": false,
-        "enableProfileMatchTool": true,
-        "source": [
-            {
-                "id": 0,
-                "type": "LAST_MODIFIED_BY",
-                "firstName": "Korn",
-                "lastName": "Ferry",
-                "effectiveDateTime": 1517443200000
-            },
-            {
-                "id": 0,
-                "type": "CREATED_BY",
-                "firstName": "Korn",
-                "lastName": "Ferry",
-                "effectiveDateTime": 1517443200000
+function readJSONFromFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
+            if (!err) {
+                resolve({ status: 'success', data: JSON.parse(data) });
+            } else {
+                reject({ status: 'error', error: err });
             }
-        ]
-    };
-    const output = encrypt(input);
-    console.log('output', output);   
-}
-
-function main() {
-    const filePath = path.join(__dirname + '/data/success_profile_search_results.json');
-
-    fs.readFile(filePath, { encoding: 'utf-8' }, (err, data) => {
-        if (!err) {
-            const jsonifiedData = JSON.parse(data);
-            const output = [];
-            jsonifiedData.forEach((datum) => {
-                const encryptedDatum = encrypt(datum);
-                output.push(encryptedDatum);
-            });
-            console.log('output', output);
-        } else {
-            console.log('Error', err);
-        }
+        });
     });
 }
 
-// mockMain();
+function writeJSONToFile(filePath, data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filePath, JSON.stringify(data), (err) => {
+            if (!err) {
+                resolve({ status: 'success' });
+            } else {
+                reject({ status: 'error', error: err });
+            }
+        })
+    });
+}
+
+async function main() {
+    const readResponse = await readJSONFromFile(INPUT_PATH);
+    const output = [];
+    const data = readResponse.data;
+    data.forEach((datum) => {
+        const encryptedDatum = encrypt(datum);
+        output.push(encryptedDatum);
+    });
+    
+    const writeResponse = await writeJSONToFile(OUTPUT_PATH, output);
+}
+
 main();
